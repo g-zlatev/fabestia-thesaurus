8cd5b01eb28b71f11483a739cb22cc5333b6b122
G.Zlatev <58149328+g-zlatev@users.noreply.github.com>
2021-12-12 02:41:19 +0200

    failed attempt to fix front page bug

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,35 +2,44 @@ import "./Intro.css";
 import IntroItem from "./IntroItem";
 import { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
-
 import db from "../../firebase";
 
 function Intro() {
-  // let intros2 = [
-  //   {
-  //     id: 5,
-  //     title: "Qilin",
-  //     text: "The qilin is a legendary hooved chimerical creature that appears in Chinese mythology, and is said to appear with the imminent arrival or passing of a sage or illustrious ruler.",
-  //     author: "Pesho",
-  //     bgImage: "https://wallpaperaccess.com/full/2882214.jpg",
-  //   },
-  //   {
-  //     id: 6,
-  //     title: "Test Title 2",
-  //     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, molestiae?",
-  //     author: "Author2",
-  //     bgImage:
-  //       "https://i.pinimg.com/550x/01/d1/31/01d131db9350156ba2ef8c8770998e7d.jpg",
-  //   },
-  // ];
-
   const [intros, setIntros] = useState([]);
 
+  let intros2 = [
+    {
+      id: 5,
+      title: "Qilin",
+      text: "The qilin is a legendary hooved chimerical creature that appears in Chinese mythology, and is said to appear with the imminent arrival or passing of a sage or illustrious ruler.",
+      author: "Pesho",
+      bgImage: "https://wallpaperaccess.com/full/2882214.jpg",
+    },
+    // {
+    //   id: 6,
+    //   title: "Test Title 2",
+    //   text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, molestiae?",
+    //   author: "Author2",
+    //   bgImage:
+    //     "https://i.pinimg.com/550x/01/d1/31/01d131db9350156ba2ef8c8770998e7d.jpg",
+    // },
+  ];
+
   useEffect(() => {
-    onSnapshot(collection(db, "articles2"), (snapshot) => {
-      setIntros(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    });
+    setIntros(intros2);
   }, []);
+
+  // useEffect(() => {
+  //   onSnapshot(collection(db, "articles2"), (snapshot) => {
+  //     setIntros(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   fetch("https://61b52f620e84b70017331a94.mockapi.io/articles/intros").then(
+  //     (res) => res.json().then((result) => setIntros(result))
+  //   );
+  // }, []);
 
   console.log(intros);
 
--- src/components/Intro/IntroItem.js
+++ src/components/Intro/IntroItem.js
@@ -1,5 +1,5 @@
 function IntroItem({ item }) {
-
+  
   return (
     <div
       className="intro-item"
