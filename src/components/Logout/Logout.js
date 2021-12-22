6783cdc9bbd51bd1f34938d79a95a463a0ce0396
G.Zlatev <58149328+g-zlatev@users.noreply.github.com>
2021-12-22 21:23:03 +0200

    implement Logout (not working?)

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,13 +1,18 @@
 import { Navigate } from "react-router-dom";
 import * as authService from "../../services/authService";
 
-const Logout = ({
-    onLogout
-}) => {
-    authService.logout();
-    onLogout();
-    
-  return <Navigate to={"/login"} replace={true}/>;
+const Logout = ({ onLogout }) => {
+  authService
+    .logout()
+    .then(() => {
+      console.log("logged out");
+    })
+    .catch((error) => {
+      window.alert(error);
+    });
+  // onLogout();
+
+  return <Navigate to={"/login"} replace={true} />;
 };
 
 export default Logout;
